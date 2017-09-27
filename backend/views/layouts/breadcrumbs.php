f3b48b0387e1082d4790fded9b3b5118834b69d4
Mohamad Faeez <mfaeezmazlan@gmail.com>
2017-09-28 01:48:40 +0800

    Add dump and breadcrumbs file

--- backend/views/layouts/breadcrumbs.php
+++ backend/views/layouts/breadcrumbs.php
@@ -2,20 +2,25 @@
 
 use yii\widgets\Breadcrumbs;
 ?>
-<div class="row clearfix">
-    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        <!--<div class="card">-->
-        <?=
-        Breadcrumbs::widget([
-            'options' => [
-                'class' => 'breadcrumb breadcrumb-bg-blue-grey'
-            ],
-            'homeLink' => [
-                'label' => Yii::t('yii', 'Dashboard'), 'url' => Yii::$app->homeUrl,
-                'template' => "<li><a href='javascript:history.back()'><i class='material-icons col-white'>arrow_back</i></a>{link}</li>"
-            ],
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]);
-        ?>
-    </div>
+
+<div class="breadcrumbs" id="breadcrumbs">
+    <script type="text/javascript">
+        try {
+            ace.settings.check('breadcrumbs', 'fixed')
+        } catch (e) {
+        }
+    </script>
+    <!--<div class="card">-->
+    <?=
+    Breadcrumbs::widget([
+        'options' => [
+            'class' => 'breadcrumb breadcrumb-bg-blue-grey'
+        ],
+        'homeLink' => [
+            'label' => Yii::t('yii', 'Dashboard'), 'url' => Yii::$app->homeUrl,
+            'template' => "<li><a href='javascript:history.back()'><i class='fa fa-home fa-large blue'></i></a> {link}</li>"
+        ],
+        'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+    ]);
+    ?>
 </div>
