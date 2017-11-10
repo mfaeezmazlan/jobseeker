d15a813aa4fcc9525dfec15ec65f6b53e48d7447
Mohamad Faeez <mfaeezmazlan@gmail.com>
2017-11-11 00:48:04 +0800

    Update generic web model at common for Cron Usage friendly

--- common/models/GenericWeb.php
+++ common/models/GenericWeb.php
@@ -8,91 +8,109 @@ use backend\models\User as BackendUser;
 class GenericWeb extends \yii\db\ActiveRecord {
 
     public function beforeSave($insert) {
-        if (Yii::$app->user->isGuest) {
-            if (parent::beforeSave($insert)) {
-                if (!defined('STDIN')) {
-                    $user = BackendUser::find()->where("username='guest'")->one();
-                }
-                if ($this->isNewRecord) {
-                    $this->updated_at = $this->created_at = date('Y-m-d H:i:s');
-                    if (!defined('STDIN')) {
-                        $this->updated_by = $this->created_by = Yii::$app->user->id;
-                    } else {
-                        $this->updated_by = -99;
-                    }
-                } else {
-                    //$this->created_at= date('Y-m-d H:i:s'); 
-                    if ($this->isDeleted == "1") {
-                        $this->deleted_at = date('Y-m-d H:i:s');
-                        if (!empty($user)) {
-                            if (!defined('STDIN')) {
-                                $this->deleted_by = Yii::$app->user->id;
-                            } else {
-                                $this->deleted_by = -99;
-                            }
-                        } else {
-                            $this->deleted_by = -99;
-                        }
-                    } else {
-                        $this->updated_at = date('Y-m-d H:i:s');
-                        if (!empty($user)) {
-                            if (!defined('STDIN')) {
-                                $this->updated_by = Yii::$app->user->id;
-                            } else {
-                                $this->updated_by = -99;
-                            }
-                        } else {
-                            $this->updated_by = -99;
-                        }
-                    }
-                }
-                $user = null;
-                return true;
+
+        if (defined('STDIN')) {
+            if ($this->isNewRecord) {
+                $this->updated_at = $this->created_at = date('Y-m-d H:i:s');
+                $this->updated_by = 0;
             } else {
-                return false;
+                //$this->created_at= date('Y-m-d H:i:s'); 
+                if ($this->isDeleted == "1") {
+                    $this->deleted_at = date('Y-m-d H:i:s');
+                    $this->deleted_by = 0;
+                } else {
+                    $this->updated_by = 0;
+                }
             }
+            $user = null;
+            return true;
         } else {
-            if (parent::beforeSave($insert)) {
-                if (!defined('STDIN')) {
-                    $user = BackendUser::find()->where("username='" . Yii::$app->user->identity->username . "'")->one();
-                }
-                if ($this->isNewRecord) {
-                    $this->updated_at = $this->created_at = date('Y-m-d H:i:s');
+            if (Yii::$app->user->isGuest) {
+                if (parent::beforeSave($insert)) {
                     if (!defined('STDIN')) {
-                        $this->updated_by = $this->created_by = Yii::$app->user->id;
-                    } else {
-                        $this->updated_by = -99;
+                        $user = BackendUser::find()->where("username='guest'")->one();
                     }
+                    if ($this->isNewRecord) {
+                        $this->updated_at = $this->created_at = date('Y-m-d H:i:s');
+                        if (!defined('STDIN')) {
+                            $this->updated_by = $this->created_by = Yii::$app->user->id;
+                        } else {
+                            $this->updated_by = 0;
+                        }
+                    } else {
+                        //$this->created_at= date('Y-m-d H:i:s'); 
+                        if ($this->isDeleted == "1") {
+                            $this->deleted_at = date('Y-m-d H:i:s');
+                            if (!empty($user)) {
+                                if (!defined('STDIN')) {
+                                    $this->deleted_by = Yii::$app->user->id;
+                                } else {
+                                    $this->deleted_by = 0;
+                                }
+                            } else {
+                                $this->deleted_by = 0;
+                            }
+                        } else {
+                            $this->updated_at = date('Y-m-d H:i:s');
+                            if (!empty($user)) {
+                                if (!defined('STDIN')) {
+                                    $this->updated_by = Yii::$app->user->id;
+                                } else {
+                                    $this->updated_by = 0;
+                                }
+                            } else {
+                                $this->updated_by = 0;
+                            }
+                        }
+                    }
+                    $user = null;
+                    return true;
                 } else {
-                    //$this->created_at= date('Y-m-d H:i:s'); 
-                    if ($this->isDeleted == "1") {
-                        $this->deleted_at = date('Y-m-d H:i:s');
-                        if (!empty($user)) {
-                            if (!defined('STDIN')) {
-                                $this->deleted_by = Yii::$app->user->id;
-                            } else {
-                                $this->deleted_by = -99;
-                            }
+                    return false;
+                }
+            } else {
+                if (parent::beforeSave($insert)) {
+                    if (!defined('STDIN')) {
+                        $user = BackendUser::find()->where("username='" . Yii::$app->user->identity->username . "'")->one();
+                    }
+                    if ($this->isNewRecord) {
+                        $this->updated_at = $this->created_at = date('Y-m-d H:i:s');
+                        if (!defined('STDIN')) {
+                            $this->updated_by = $this->created_by = Yii::$app->user->id;
                         } else {
-                            $this->deleted_by = -99;
+                            $this->updated_by = 0;
                         }
                     } else {
-                        $this->updated_at = date('Y-m-d H:i:s');
-                        if (!empty($user)) {
-                            if (!defined('STDIN')) {
-                                $this->updated_by = Yii::$app->user->id;
+                        //$this->created_at= date('Y-m-d H:i:s'); 
+                        if ($this->isDeleted == "1") {
+                            $this->deleted_at = date('Y-m-d H:i:s');
+                            if (!empty($user)) {
+                                if (!defined('STDIN')) {
+                                    $this->deleted_by = Yii::$app->user->id;
+                                } else {
+                                    $this->deleted_by = 0;
+                                }
                             } else {
-                                $this->updated_by = -99;
+                                $this->deleted_by = 0;
                             }
                         } else {
-                            $this->updated_by = -99;
+                            $this->updated_at = date('Y-m-d H:i:s');
+                            if (!empty($user)) {
+                                if (!defined('STDIN')) {
+                                    $this->updated_by = Yii::$app->user->id;
+                                } else {
+                                    $this->updated_by = 0;
+                                }
+                            } else {
+                                $this->updated_by = 0;
+                            }
                         }
                     }
+                    $user = null;
+                    return true;
+                } else {
+                    return false;
                 }
-                $user = null;
-                return true;
-            } else {
-                return false;
             }
         }
     }
